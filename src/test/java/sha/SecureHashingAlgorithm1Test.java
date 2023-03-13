fac1ef279a9f27466fa7eb41a4117a41034b1044
Henrik Klasen <henrik.klasen.001@student.uni.lu>
2023-03-13 12:35:29 +0100

    added initialization method to SecureHashingAlgorithm1Test.java

--- src/test/java/sha/SecureHashingAlgorithm1Test.java
+++ src/test/java/sha/SecureHashingAlgorithm1Test.java
@@ -1,5 +1,15 @@
 package sha;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class SecureHashingAlgorithm1Test {}
+public class SecureHashingAlgorithm1Test {
+    
+
+    @BeforeEach
+    private void init(){
+
+    }
+}
